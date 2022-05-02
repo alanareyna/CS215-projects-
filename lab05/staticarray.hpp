4d10a5665fbb71600a6d5c622e27d875ca6bef13
Alana Reyna <60456837+alanareyna@users.noreply.github.com>
2022-05-01 20:52:59 -0700

    Create staticarray.hpp

---
+++ lab05/staticarray.hpp
@@ -0,0 +1,18 @@
+const int MAX = 10;
+const int notvalid = -65342;
+
+class StaticArray {
+   public:
+       StaticArray();
+       bool append(int value);
+  int at(int index);
+  int sum();
+  bool remove(int value);
+      ~StaticArray() { } // destructor - ignore for now                                                                                                                                                     
+      void print();
+
+   private:
+      int arr[MAX];
+  int len = 0;
+
+};
